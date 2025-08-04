1bffc3208bd760dd7b8cd1028396d5441177bd46
Michel Boucey <michel.boucey@gmail.com>
2025-08-04 14:54:52 +0200

    Add comments to build.sh

--- script/build.sh
+++ script/build.sh
@@ -1,9 +1,11 @@
+# Set an Haskell Git repo URL that can deliver
+# a binary just by running a cabal install command inside
 HASKELL_GIT_REPO=https://github.com/MichelBoucey/ip6addr.git
 
-# Below this line, it should be leaved unchanged
+# Below this line, it should be leaved usually unchanged
 
 cd /tmp
 git clone ${HASKELL_GIT_REPO} cloned-repo
 cd cloned-repo
-# The binary artefact have to be copied to /tmp/bin/
+# The just built binary artefact have to be finally copied to /tmp/bin/
 cabal --enable-executable-static --install-method=copy --installdir=/tmp/bin install
