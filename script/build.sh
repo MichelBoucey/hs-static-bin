78377f8781b60e1c80aa63158676bff6cb39f240
Michel Boucey <michel.boucey@gmail.com>
2025-08-08 19:39:23 +0200

    Build.sh enhanced

--- script/build.sh
+++ script/build.sh
@@ -4,8 +4,13 @@ git clone $1 cloned-repo
 
 cd cloned-repo
 
-# The just-built static binary artifact have to be copied to /tmp/bin/
-cabal --enable-executable-static --install-method=copy --installdir=/tmp/bin install
+# The just-built static binary artifact
+# have to be copied to /tmp/bin/
+cabal --enable-executable-static \
+      --overwrite-policy=always \
+      --install-method=copy \
+      --installdir=/tmp/bin \
+      install
 
 # Set the right ownership
 chown $CURUID:$CURGID /tmp/bin/*
