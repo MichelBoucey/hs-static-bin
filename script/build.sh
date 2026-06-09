ea895f5339d3c7b7e757804d128d69411f827bdd
Michel Boucey <michel.boucey@gmail.com>
2026-06-09 16:28:16 +0200

    Add missing 'cabal update'

--- script/build.sh
+++ script/build.sh
@@ -1,11 +1,15 @@
 #!/bin/ash
 
+# To be sure to use
+# the latest packages
+cabal update
+
 cd /tmp
 
 echo "Cloning $HASKELL_GIT_REPO_URL"
 git clone $HASKELL_GIT_REPO_URL cloned-repo
 
-cd cloned-repo
+cd cloned-repo/$HASKELL_CABAL_SUB_PROJECT
 
 # The just-built static binary artifact
 # have to be copied to /tmp/bin/
